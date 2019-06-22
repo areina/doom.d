e6eb2dcd7ee9160313571ae12b75967d11bc8526
Toni Reina <treina@newrelic.com>
2019-06-22 09:24:17 +0200

    Add notmuch package for managing the email

--- packages.el
+++ packages.el
@@ -12,3 +12,4 @@
 (package! evil-matchit :recipe (:fetcher github :repo "redguardtoo/evil-matchit" :commit "7d65b4167b1f0086c2b42b3aec805e47a0d355c4"))
 (package! poet-theme)
 (package! olivetti)
+(package! notmuch)
