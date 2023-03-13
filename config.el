891e173788119535d596bd4d7160c42c32e7307a
Toni Reina <areina0@gmail.com>
2023-03-13 15:07:45 +0100

    Few random updates

--- config.el
+++ config.el
@@ -5,40 +5,61 @@
 
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
+;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Toni Reina"
       user-mail-address "areina0@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+(setq doom-font (font-spec :family "Fira Code" :size 12))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-nord)
+
+;; macosx transparent titlebar
+(when (eq system-type 'darwin)
+  (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+  (add-to-list 'default-frame-alist '(ns-appearance . dark)))
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-(add-hook 'focus-out-hook 'evil-force-normal-state)
-
-(use-package! aggressive-indent
-  :init
-  (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode))
-
-(use-package! super-save
-  :config
-  (progn
-    (super-save-mode +1)))
-
-(use-package! markdown-mode
-  :init
-  (setq markdown-hide-urls t))
-
-(after! elisp-mode
-  (setq initial-major-mode 'emacs-lisp-mode))
-
-(setq flycheck-disabled-checkers '(go-megacheck))
-
-(load! "+ui")
-(load! "+bindings")
-(load! "+org")
-(load! "+mail")
-
-;; Here are some additional functions/macros that could help you configure Doom:
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
 ;;
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package!' for configuring packages
@@ -51,8 +72,66 @@
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(provide 'config)
+(load! "+org")
+(load! "+mail")
+
+(map!
+ ;; Easier window navigation
+ :n "C-h"   #'evil-window-left
+ :n "C-j"   #'evil-window-down
+ :n "C-k"   #'evil-window-up
+ :n "C-l"   #'evil-window-right)
+
+(map!
+ (:map 'override
+  :v "v" #'er/expand-region
+  :v "V" #'er/contract-region))
+
+(defun stumpwm/connect ()
+  "Connect to the running StumpWM slynk."
+  (interactive)
+  (sly-connect "localhost" 4003)
+  (run-at-time 0.5     ;; in half second
+               nil     ;; do NOT repeat
+               (lambda ()    ;; this lambda is called
+                 (sly-mrepl-sync "stumpwm"))))
+
+(defun doom/configure-dashboard ()
+  "Configure Doom's dashboard"
+  (let ((footer-text "ALL GLORY TO THE HYPNOTOAD!")
+        (splash-image-path (expand-file-name "images/hypnotoad.png" doom-user-dir)))
+    (setq fancy-splash-image splash-image-path)
+    (remove-hook '+doom-dashboard-functions #'doom-dashboard-widget-footer)
+    (add-hook! '+doom-dashboard-functions :append
+      (insert "\n" (+doom-dashboard--center +doom-dashboard--width footer-text)))
+    (assoc-delete-all "Recently opened files" +doom-dashboard-menu-sections)
+    (assoc-delete-all "Jump to bookmark" +doom-dashboard-menu-sections)))
+
+(doom/configure-dashboard)
+
+(after! clojure-mode
+  (require 'flycheck-clj-kondo))
+
+(use-package! aggressive-indent
+  :config (global-aggressive-indent-mode t))
+
+(use-package! evil-cleverparens
+  :after evil
+  :hook (clojure-mode . evil-cleverparens-mode)
+  :init (setq evil-mode-beyond-eol t))
+
+(use-package! super-save
+  :config
+  (progn
+    (super-save-mode +1)))
+
+(defun add-cider-format-before-save ()
+  (add-hook 'before-save-hook 'cider-format-buffer t t))
+
+(add-hook 'clojure-mode-hook 'add-cider-format-before-save)
